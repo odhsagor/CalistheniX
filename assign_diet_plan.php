3da5b8dc8f6bebb94d643edfc9a712c212556c45
obidul huq sagor <72642687+odhsagor@users.noreply.github.com>
2024-12-23 21:41:56 +0600

    Commited BY @ODHSAGOR

--- assign_diet_plan.php
+++ assign_diet_plan.php
@@ -9,9 +9,9 @@ if (!isset($_SESSION['nutritionist_id'])) {
 
 // Database connection
 $servername = "localhost";
-$username = "calisthe_CalistheniX_db_user";
-$password_db = "zI3t=Pa94uG#";
-$dbname = "calisthe_CalistheniX_db";
+$username = "root";
+$password_db = "";
+$dbname = "CalistheniX_db";
 
 $conn = new mysqli($servername, $username, $password_db, $dbname);
 if ($conn->connect_error) {
